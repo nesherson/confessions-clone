32934d50b95e42180051e9270ecb09f29d909cd6
nesherson <nesad.omeragich27@gmail.com>
2021-06-19 02:03:52 +0200

    Feat: add functionality for adding, updating and removing post from db

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,8 +2,73 @@ const router = require('express').Router();
 
 const Post = require('../models/post.model');
 
-router.route('/').get((req, resp) => {
-  User.find();
+router.route('/').get((req, res) => {
+  Post.find()
+    .then((posts) => {
+      return res.json(posts);
+    })
+    .catch((err) => {
+      return res.status(404).json(`Error: ${err}`);
+    });
 });
 
-modules.exports = router;
+router.route('/add-post').post((req, res) => {
+  const body = req.body.body;
+  const likes = Number(req.body.likes);
+  const dislikes = Number(req.body.dislikes);
+  const comments = Number(req.body.comments);
+  const date = Date.parse(req.body.date);
+
+  const newPost = new Post({body, likes, dislikes, comments, date});
+  newPost.save()
+    .then(() => {
+      return res.json('Post added!');
+    })
+    .catch((err) => {
+      return res.json(`Error: ${err}`);
+    });
+});
+
+router.route('/post/:id').get((req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      return res.json(post);
+    })
+    .catch((err) => {
+      return res.status(404).json(`Error: ${err}`);
+    });
+});
+
+router.route('/post/delete/:id').delete((req, res) => {
+  Post.findByIdAndDelete(req.params.id)
+    .then(() => {
+      return res.json('Exercise Deleted!');
+    })
+    .catch((err) => {
+      return res.status(404).json(`Error: ${err}`);
+    });
+});
+
+router.route('/post/update/:id').get((req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      post.body = req.body.body;
+      post.likes = req.body.likes;
+      post.dislikes = req.body.dislikes;
+      post.date = Date.parse(req.body.date)
+
+      post.save()
+        .then(() => {
+          return res.json('Exercise Updated!');
+        })
+        .catch((err) => {
+          return res.status(404).json(`Error: ${err}`);
+        });
+    })
+    .catch((err) => {
+      return res.status(404).json(`Error: ${err}`);
+    });
+});
+
+
+module.exports = router;
