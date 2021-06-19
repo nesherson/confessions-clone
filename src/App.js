b8364fa7291ade7da61e214e4b8c723880316114
nesherson <nesad.omeragich27@gmail.com>
2021-06-20 00:44:05 +0200

    Feat: add new route for post details

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Posts from './components/Posts/Posts';
 import AddPost from './components/AddPost/AddPost'
+import PostDetails from './components/PostDetails/PostDetails';
 
 function App() {
   return (
     <Router>
       <Route path='/' exact component={Posts} />
       <Route path='/add-post' component={AddPost}/>
+      <Route path='/post/:id' component={PostDetails}/>
     </Router>
   );
 }
