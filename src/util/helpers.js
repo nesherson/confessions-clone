01693a0ad1b0e1e70bb966cbd4e9f49b578f1ee1
unknown <nesad.omeragich27@gmail.com>
2021-06-22 03:51:21 +0200

    Refactor: exported parseDate to its own file

---
+++ src/util/helpers.js
@@ -0,0 +1,8 @@
+export const parseDate = (date) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  return new Date(date).toLocaleDateString('en-GB', options);
+};
