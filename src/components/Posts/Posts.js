4d05691dea8aa020544a3af3e940bcd337dc09bc
unknown <nesad.omeragich27@gmail.com>
2021-06-22 03:53:04 +0200

    Style: add style

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,30 +2,17 @@ import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Header = styled.header`
-  height: 54px;
-  border-bottom: 1px solid #000;
-`;
+import Post from '../../UI/Post/Post';
 
 const Main = styled.main`
   width: 100%;
   height: calc(100vh - 54px);
 `;
 const PostList = styled.ul`
-  width: 450px;
-  border: 1px solid #000;
+  width: 560px;
   margin: 0 auto;
   list-style: none;
   padding: 15px 25px;
-`;
-
-const ListItem = styled.li`
-  margin: 15px 0 25px 0;
-  border-bottom: 1px solid #ccc;
-`;
-
-const ListHeader = styled.h2`
-  text-align: center;
 `;
 
 const fetchData = async (url = '') => {
@@ -52,13 +39,20 @@ const Posts = () => {
   const history = useHistory();
 
   useEffect(() => {
+    let isMounted = true;
     fetchData('http://localhost:5000/')
       .then((res) => {
-        setPosts(res);
+        if (isMounted) {
+          setPosts(res);
+        }
       })
       .catch((err) => {
         console.log('Posts/fetchData - err: ', err);
       });
+
+    return () => {
+      isMounted = false;
+    };
   }, [posts]);
 
   const handlePostDetails = (id) => {
@@ -66,7 +60,7 @@ const Posts = () => {
   };
 
   const handlePostLike = (id) => {
-    postData(`http://localhost:5000/post/update/${id}?like=true`)
+    postData(`http://localhost:5000/post/${id}/like`)
       .then((res) => {
         console.log('Posts/handleLike - res: ', res);
       })
@@ -76,53 +70,34 @@ const Posts = () => {
   };
 
   const handlePostDislike = (id) => {
-    postData(`http://localhost:5000/post/update/${id}?dislike=true`)
+    postData(`http://localhost:5000/post/${id}/dislike`)
       .then((res) => {
-        console.log('Posts/handleLike - res: ', res);
+        console.log('Posts/handlePostDislike - res: ', res);
       })
       .catch((err) => {
-        console.log('Posts/handleLike - err: ', err);
+        console.log('Posts/handlePostDislike - err: ', err);
       });
   };
 
   return (
     <div>
-      <Header>
-        <h1>Confessions</h1>
-      </Header>
       <Main>
-        <ListHeader>List of posts:</ListHeader>
         <PostList>
           {posts
             ? posts.map((post) => {
                 return (
-                  <ListItem key={post._id}>
-                    <p>{post.body}</p>
-                    <span>Likes: {post.likes} </span>
-                    <button
-                      onClick={() => {
-                        handlePostLike(post._id);
-                      }}
-                    >
-                      Like
-                    </button>
-                    <span>Dislikes: {post.dislikes}</span>
-                    <button
-                      onClick={() => {
-                        handlePostDislike(post._id);
-                      }}
-                    >
-                      Dislike
-                    </button>
-                    <span>Comments: {post.comments.length}</span>
-                    <button
-                      onClick={() => {
-                        handlePostDetails(post._id);
-                      }}
-                    >
-                      Comment
-                    </button>
-                  </ListItem>
+                  <Post
+                    key={post._id}
+                    id={post._id}
+                    date={post.date}
+                    likes={post.likes}
+                    dislikes={post.dislikes}
+                    comments={post.comments.length}
+                    like={handlePostLike}
+                    dislike={handlePostDislike}
+                  >
+                    {post.body}
+                  </Post>
                 );
               })
             : null}
